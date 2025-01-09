f471038c08946cefa2a80bd32505b25b8f6edcae
okaya <kayaogz@gmail.com>
2025-01-09 10:58:33 +0100

    corrections tp1 openmp

---
+++ tp/tp1-openmp/correction/hello-ref.cpp
@@ -0,0 +1,20 @@
+#include <cstdio>
+#include <iostream>
+#include "omp.h"
+
+int main()
+{
+  omp_set_num_threads(5);
+#pragma omp parallel num_threads(7)
+  {
+    printf("Thread %d/%d\n", omp_get_thread_num(), omp_get_num_threads());
+#pragma omp sections
+    {
+#pragma omp section
+      {
+        printf("Hello depuis thread %d\n", omp_get_thread_num());
+      }
+    }
+  }
+  return 0;
+}
